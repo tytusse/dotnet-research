9606cb4bda8b83ce8a8b7d4e3f54b09c7b125c59
Tomasz Sztokinier <tomasz@todoit.pl>
2022-09-08 16:30:35 +0200

    separate ordered mailbox poc into separate file

--- ConsoleMailboxPriorities/Program.fs
+++ ConsoleMailboxPriorities/Program.fs
@@ -1,15 +1,8 @@
 ﻿open System
+open ConsoleMailboxPriorities
 
 type MyMessage = {Id:int}
 
-type Work<'Prio> = {
-    Run:unit->unit
-    Priority:'Prio
-}
-
-type PriorityManagerMessage<'Prio> =
-    | RunnerDone
-    | QueueWork of Work<'Prio>
 let jobLengthMs = 1000
 let postTimeoutMs = 5000
 let start = DateTime.Now
@@ -23,45 +16,16 @@ let logger = MailboxProcessor.Start(fun mailbox ->
     }
     loop())
 
-let buffer = MailboxProcessor.Start(fun mailbox ->
-    let rec loop (isBusy:bool) waiting n = async {
-        let! (msg:PriorityManagerMessage<_>) = mailbox.Receive()
-        logger.Post $"[{n}] enqueued: {msg}, isBusy: {isBusy} waiting: {waiting}"
-        
-        let runWithPriority (waiting:Work<_> list) =
-            logger.Post $"runWithPriority {waiting}"
-            match waiting |> List.sortBy(fun x -> x.Priority) with
-            | work :: waiting ->
-                work.Run()
-                loop true waiting (n+1)
-            | [] -> loop isBusy [] (n+1)
-        
-        match msg with
-        | QueueWork work ->
-            let waiting = work::waiting
-            if isBusy
-            then return! loop isBusy waiting (n+1)
-            else return! runWithPriority waiting
-        | RunnerDone -> return! runWithPriority waiting       
-    }
-    loop false [] 0)
-
-let runner = MailboxProcessor.Start(fun mailbox ->
-    let rec loop n = async {
-        let! (msg:MyMessage) = mailbox.Receive()
-        logger.Post $"runner[{n}]handling msg [{msg.Id}]"
-        do! Async.Sleep jobLengthMs
-        logger.Post $"runner[{n}]handled msg [{msg.Id}]"
-        buffer.Post RunnerDone
-        return! loop (n+1)
-    }
-    loop 0)
+let runner = TwoLoopsMailboxes.Agent<MyMessage>(
+    logger.Post,
+    (fun item -> (item.Id % 2, item.Id)),
+    (fun _ -> Async.Sleep jobLengthMs))
 
 for i=1 to 10 do
     printfn $"posting {i}"
     let item = {Id=i}
     let priority = (item.Id % 2, item.Id)
-    buffer.Post(QueueWork {Run = (fun() -> runner.Post(item)); Priority=priority})
+    runner.Post(item)
     
 System.Console.ReadKey() |> ignore
 
---
+++ ConsoleMailboxPriorities/TwoLoopsMailboxes.fs
@@ -0,0 +1,51 @@
+﻿module ConsoleMailboxPriorities.TwoLoopsMailboxes
+
+open System
+
+type Work = {
+    Run:unit->unit
+    Priority:IComparable
+}
+
+type Command<'T> =
+    | RunnerDone
+    | QueueWork of 'T
+
+type Agent<'T>(logger:string->unit, getPrio:'T->IComparable, run:'T->Async<unit>) =
+    let buffer = MailboxProcessor.Start(fun mailbox ->
+        let rec loop (isBusy:bool) waiting n = async {
+            let! (msg:Command<_>) = mailbox.Receive()
+            logger $"[{n}] enqueued: {msg}, isBusy: {isBusy} waiting: {waiting}"
+            
+            let runWithPriority (waiting:Work list) =
+                logger $"runWithPriority {waiting}"
+                match waiting |> List.sortBy(fun x -> x.Priority) with
+                | work :: waiting ->
+                    work.Run()
+                    loop true waiting (n+1)
+                | [] -> loop isBusy [] (n+1)
+            
+            match msg with
+            | QueueWork work ->
+                let waiting = work::waiting
+                if isBusy
+                then return! loop isBusy waiting (n+1)
+                else return! runWithPriority waiting
+            | RunnerDone -> return! runWithPriority waiting       
+        }
+        loop false [] 0)
+
+    let runner = MailboxProcessor.Start(fun mailbox ->
+        let rec loop n = async {
+            let! (msg:'T) = mailbox.Receive()
+            logger $"runner[{n}]handling msg [{msg}]"
+            do! run msg
+            logger $"runner[{n}]handled msg [{msg}]"
+            buffer.Post RunnerDone
+            return! loop (n+1)
+        }
+        
+        loop 0)
+    
+    member _.Post(item) =
+        buffer.Post(QueueWork {Run = (fun() -> runner.Post(item)); Priority=getPrio item})
