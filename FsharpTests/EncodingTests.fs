0a0019981c04598b0058d4361ceed6908a5c6f94
Tomasz Sztokinier <tomasz@todoit.pl>
2023-11-13 14:29:21 +0100

    BOM differences when using StreamWriter vs. Encoding.GetBytes()

---
+++ FsharpTests/EncodingTests.fs
@@ -0,0 +1,27 @@
+﻿module FsharpTests.EncodingTests
+
+open System.IO
+open System.Text
+open Xunit
+open Xunit.Abstractions
+let bomBytesUtf8 = [| 0xEFuy;0xBBuy;0xBFuy |]
+let encoding = Encoding.UTF8
+let defaultBufferSize = 1024
+
+type Fixture(hlp:ITestOutputHelper) =
+    [<Fact>]
+    member _.``Stream writing adds BOM``() :unit =
+        use stream = new MemoryStream()
+        use wr = new StreamWriter(stream, encoding, defaultBufferSize, true)
+        wr.Write("foobar")
+        wr.Flush()
+        let arr = stream.ToArray()
+        hlp.WriteLine($"Result: %A{arr}")
+        Assert.Equal<byte>(bomBytesUtf8, arr[0..2])
+        
+    [<Fact>]
+    member _.``GetBytes does NOT add BOM``() :unit =
+        let arr = encoding.GetBytes("foobar")
+        hlp.WriteLine($"Result: %A{arr}")
+        Assert.NotEqual<byte>(bomBytesUtf8, arr[0..2])
+        
