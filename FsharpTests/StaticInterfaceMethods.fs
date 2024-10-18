f670d5a9f7fafe758c1f74519ec43c395ea6e700
Tomasz Sztokinier <tomasz@todoit.pl>
2024-10-18 14:52:09 +0200

    F# static interface method compiler checks bug leading to runtime error

---
+++ FsharpTests/StaticInterfaceMethods.fs
@@ -0,0 +1,30 @@
+﻿module FsharpTests.StaticInterfaceMethods
+
+#nowarn "3535" "3536"
+
+open Xunit
+
+type IWithStatics =
+    abstract Id: int
+    static abstract TranslateFromEnglish: word:string -> string
+    
+type Dog() =
+    interface IWithStatics with
+        member _.Id = 42
+        static member TranslateFromEnglish (word:string) = $"{word}, woof! woof!"
+        
+        
+let resolve<'Animal & #IWithStatics>(word:string) : string =
+    'Animal.TranslateFromEnglish(word)
+    
+type Fixture() =
+    [<Fact>]
+    member _.``Dog.TranslateFromEnglish bad``() =
+        Assert.Equal("hello, woof! woof!", resolve("hello"))
+        
+    [<Fact>]
+    member _.``Dog.TranslateFromEnglish good``() =
+        Assert.Equal("hello, woof! woof!", resolve<Dog>("hello"))
+        
+        
+        
