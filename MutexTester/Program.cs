e39518ade8ac5b98d6d14e36f4f45e634ec451b5
Tomasz Sztokinier <tomasz@todoit.pl>
2024-10-18 14:36:46 +0200

    testing mutex

---
+++ MutexTester/Program.cs
@@ -0,0 +1,18 @@
+﻿
+UseMutexAndWaitForInput();
+
+void UseMutexAndWaitForInput()
+{
+    //using var mutex = new Mutex(false, @"Global\MyMutex");
+    using var mutex = new Mutex(false, @"Global\MyMutex");
+    using var localMutex = new Mutex(false, @"MyMutex.TheSecond");
+    
+    if (!WaitHandle.WaitAll([mutex, localMutex], 1000))
+    {
+        Console.WriteLine("Another instance of the app is running. Exiting.");
+        return;
+    }
+
+    Console.WriteLine("Running the app. Press any key to exit.");
+    Console.ReadKey();
+}
