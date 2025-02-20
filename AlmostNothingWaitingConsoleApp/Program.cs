9a22b66fc430b81a77ec666b8ef247eaec710718
Tomasz Sztokinier <tomasz@todoit.pl>
2025-02-20 11:53:48 +0100

    Bare console app to see dotnet RAM overhead in mem profiler/windbg

---
+++ AlmostNothingWaitingConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World! Press any key to quit");
+var key = Console.ReadKey();
+Console.WriteLine($"Pressed key: {key.Key}. Quitting.");
