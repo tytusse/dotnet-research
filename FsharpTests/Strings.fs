4b2a3f9a3d4f70491e1fe2ee1d71dbfb8dccb63e
Tomasz Sztokinier <tomasz@todoit.pl>
2025-02-28 15:11:53 +0100

    F# strings

---
+++ FsharpTests/Strings.fs
@@ -0,0 +1,16 @@
+﻿module FsharpTests.Strings
+
+open System.Text.RegularExpressions
+open Xunit
+open Xunit.Abstractions
+
+type Tests(hlp:ITestOutputHelper) =
+    
+    [<Fact>]
+    member _.``String triple quote formatting visual test``() : unit =
+        let text =
+            """
+            select StoreID
+            """
+        let verbose = Regex.Replace(text, @"[ \t]", "*")
+        hlp.WriteLine verbose
