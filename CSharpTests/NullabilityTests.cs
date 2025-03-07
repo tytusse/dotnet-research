8f1d00b6b9c8e9af9f7d754697ca03d1db56340d
Tomasz Sztokinier <tomasz@todoit.pl>
2025-03-07 16:01:23 +0100

    C# nullability

---
+++ CSharpTests/NullabilityTests.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpTests;
+
+public class NullabilityTests {
+    public enum TestEnum {
+        Foo, Bar, Baz
+    }
+    
+    
+    [Fact]
+    public void Enumbrable_Cast_Nullability() {
+        var vals =  Enum.GetValues<TestEnum>();
+    }
+}
