980b0cfbff1042d509272992958f336f6f17f5fe
Tomasz Sztokinier <tomasz@todoit.pl>
2025-03-17 17:22:40 +0100

    XUnit and "magic" array asserts
    XUnit `Assert.Equals<T>(T e, T a)` uses dynamic type checks/reflection to infer that object is an IEnumerable and thus performs "deep" compare for arrays

---
+++ CSharpTests/XUnitAsserts.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpTests;
+
+public class XUnitAsserts {
+    [Fact]
+    public void ArrayEqualWithStandardComparer() {
+        int[][] actual = [[1, 2, 3], [4, 5, 6]];
+        
+        Assert.Equal<int[][]>([[1, 2, 3], [4, 5, 6]], actual);
+    }
+}
