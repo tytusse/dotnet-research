7d9a4f3f8487b1ad17d4e73b892edeb2b3a33a02
Tomasz Sztokinier <tomasz@todoit.pl>
2025-03-17 17:38:41 +0100

    enumerable checks

---
+++ CSharpTests/Enumerable.cs
@@ -0,0 +1,29 @@
+﻿using Xunit.Abstractions;
+
+namespace CSharpTests;
+
+public class Enumerable(ITestOutputHelper helper) {
+    [Fact]
+    public void EmptyCollectionMoveNextReturnsFalse() {
+        var empty = Array.Empty<int>();
+        using var enumerator = (empty as IEnumerable<int>).GetEnumerator();
+        Assert.False(enumerator.MoveNext());
+    }
+    
+    [Fact]
+    public void CollectionCurrentWillCrashIfMoveNextNotCalled() {
+        int[] items = [1, 2, 45];
+        using var enumerator = (items as IEnumerable<int>).GetEnumerator();
+        var exception = Assert.Throws<InvalidOperationException>(() => enumerator.Current);
+        helper.WriteLine($"Exception is {exception}");
+    }
+    
+    [Fact]
+    public void EmptyCollectionCurrentWillCrashAfterFirstMoveNext() {
+        int[] empty = [];
+        using var enumerator = (empty as IEnumerable<int>).GetEnumerator();
+        enumerator.MoveNext(); // ignore
+        var exception = Assert.Throws<InvalidOperationException>(() => enumerator.Current);
+        helper.WriteLine($"Exception is {exception}");
+    }
+}
