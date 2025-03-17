82e2233349b1b317378dc23ac59ad03a0a362da8
Tomasz Sztokinier <tomasz@todoit.pl>
2025-03-17 16:51:19 +0100

    C# collection initializers

---
+++ CSharpTests/CollectionInitializers.cs
@@ -0,0 +1,11 @@
+﻿using Xunit.Abstractions;
+
+namespace CSharpTests;
+
+public class CollectionInitializers(ITestOutputHelper testOutputHelper) {
+    [Fact]
+    public void TypeOfEnumerableCollectionInitializer() {
+        IEnumerable<int> enumerable = [1, 2, 3];
+        testOutputHelper.WriteLine(enumerable.GetType().FullName);
+    }
+}
