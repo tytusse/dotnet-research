7f13b8f2262deffa247ef4a7dc0b03629c705b60
Tomasz Sztokinier <tomasz@todoit.pl>
2025-04-01 14:02:18 +0200

    disposable vs exception handling in F#

---
+++ FsharpTests/Disposable.fs
@@ -0,0 +1,16 @@
+﻿module FsharpTests.Disposable
+
+open System
+open Xunit
+open Xunit.Abstractions
+
+type Fixture(hlp:ITestOutputHelper) =
+    [<Fact>]
+    member _.``behavior of try-catch with failing disposable``() : unit =
+        let makeFailingDisposable() = { new IDisposable with
+            member _.Dispose() = failwith "dispose test fail" }
+        try
+            use d = makeFailingDisposable()
+            failwith "regular fail"
+        with x ->
+            hlp.WriteLine $"error message: {x.Message}"
