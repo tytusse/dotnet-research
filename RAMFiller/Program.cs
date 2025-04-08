9cb06a76f806de0e7feed0702aa5e619b3cb8715
Tomasz Sztokinier <tomasz@todoit.pl>
2025-04-08 13:03:12 +0200

    apps to test memory and GC

---
+++ RAMFiller/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var pleaseRun = true;
+// ReSharper disable once NotAccessedVariable
+// ReSharper disable once CollectionNeverQueried.Local
+var arrays = new List<string[]>();
+const long mega = 1000 * 1000;
+var commands = new Dictionary<string, Action<ReadOnlySpan<string>>> {
+    ["quit"]= _ => pleaseRun = false,
+    ["exit"]= _ => pleaseRun = false,
+    ["walk"]= _ => {
+        for (int arrNum = 0; arrNum < arrays.Count; arrNum++) {
+            for (int i = 0; i < arrays[arrNum].Length; i++) {
+                var x = arrays[arrNum][i];
+                
+            }
+        }
+    },
+    ["add"]= args => {
+        var megaRaw = "";
+        if (args.Length == 0) {
+            while (megaRaw == "") {
+                Console.WriteLine("How many Megs to allocate?");
+                megaRaw = Console.ReadLine() ?? "";    
+            }
+        }
+        else {
+            megaRaw = args[0];
+        }
+        
+        if (!int.TryParse(megaRaw, out var megaToAllocate) || megaToAllocate < 0) {
+            Console.WriteLine("Invalid number - please provide positive integer");
+            return;
+        }
+        
+        Console.WriteLine($"Allocating {megaToAllocate} Megs of objects");
+        var i = 0;
+        for (; i < megaToAllocate; i++) {
+            try {
+                var gigaArray = new string[mega];
+                gigaArray.Initialize();
+                arrays.Add(gigaArray);
+            }
+            catch (OutOfMemoryException) {
+                Console.WriteLine("oops - out of memory");
+                break;
+            }
+        }
+        Console.WriteLine($"Allocated {i} Megs objects");
+    },
+    ["clear"]= _ => {
+        arrays.Clear();
+        GC.Collect();
+    },
+};
+
+Console.WriteLine("GC.GetConfigurationVariables()");
+var gcConfigurationVariables = GC.GetConfigurationVariables();
+foreach (var param in gcConfigurationVariables) {
+    Console.WriteLine(param.Key + " = " + param.Value);
+}
+
+while (pleaseRun) {
+    Console.WriteLine($"Available commands: {string.Join(",", commands.Keys)}");
+    Console.WriteLine("Please enter command");
+    var commandRaw = Console.ReadLine();
+    if (commandRaw is null) {
+        pleaseRun = false;
+        continue;
+    }
+    var commandParts = 
+        commandRaw.Split(" ", StringSplitOptions.TrimEntries|StringSplitOptions.RemoveEmptyEntries);
+    var commandName = commandParts[0];
+    switch (commands.GetValueOrDefault(commandName)) {
+        case null: Console.WriteLine($"Unknown command: {commandName}"); break;
+        case var command: command(commandParts.AsSpan()[1..]); break;  
+    }
+}
+
+Console.WriteLine("Bye");
---
+++ RamAbuser/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+var list = new LinkedList<string>();
+var pleaseRun = true;
+var commands = new Dictionary<string, Action<ReadOnlySpan<string>>> {
+    ["quit"]= _ => pleaseRun = false,
+    ["exit"]= _ => pleaseRun = false,
+    ["add"]= args => {
+        const int stringSize = 1000;
+        const int million = 1000 * 1000;
+        uint megs = 1;
+        if (args.Length >= 1) {
+            if(!uint.TryParse(args[0], out megs))
+            {
+                Console.WriteLine("Error: Invalid argument: expected uint number of megs");
+                return;
+            }
+        }
+        Console.WriteLine($"will add {megs}M of big ass strings (size={stringSize}) to linked list");
+        for (var i = 0; i < megs*million; i++) {
+            // every half-million
+            if (2*i % million == 0) {
+                Console.WriteLine($"Allocated {(decimal) i / million}M strings");
+            }
+            // probably should be below LOH limit
+            list.AddLast(new string('4', stringSize));
+        }
+    },
+    ["add-small"]= _ => list.AddLast("small string"),
+    ["clear"]= _ => list.Clear(),
+    ["gccollect"]= _ => GC.Collect(),
+    ["clear-and-collect"]= _ => {
+        list.Clear();
+        GC.Collect();
+    },
+};
+
+Console.WriteLine("GC.GetConfigurationVariables()");
+var gcConfigurationVariables = GC.GetConfigurationVariables();
+foreach (var param in gcConfigurationVariables) {
+    Console.WriteLine(param.Key + " = " + param.Value);
+}
+
+
+while (pleaseRun) {
+    Console.WriteLine($"Available commands: {string.Join(",", commands.Keys)}");
+    Console.WriteLine("Please enter command");
+    var commandRaw = Console.ReadLine();
+    if (commandRaw is null) {
+        pleaseRun = false;
+        continue;
+    }
+    var commandParts = 
+        commandRaw.Split(" ", StringSplitOptions.TrimEntries|StringSplitOptions.RemoveEmptyEntries);
+    var commandName = commandParts[0];
+    switch (commands.GetValueOrDefault(commandName)) {
+        case null: Console.WriteLine($"Unknown command: {commandName}"); break;
+        case var command: command(commandParts.AsSpan()[1..]); break;  
+    }
+}
+
+Console.WriteLine("Bye");
+
+
+
+
