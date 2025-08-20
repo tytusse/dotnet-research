877e8607bf30765c726e9cd50ecbe7d2e39c6d2e
Tomasz Sztokinier <tomasz@todoit.pl>
2025-08-20 15:54:52 +0200

    F#: exception tests

---
+++ FsharpTests/Exceptions.fs
@@ -0,0 +1,15 @@
+﻿module FsharpTests.Exceptions
+
+open Xunit
+
+type Fixture() =
+    [<Fact>]
+    member _.``reraise() in finally``() : unit =
+        let test() : unit =
+            try failwith "test error"
+            with x ->
+                try ()
+                finally reraise()
+                
+        let z = Assert.Throws<exn>(test)
+        Assert.Equal("test error", z.Message)            
