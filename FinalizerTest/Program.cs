c7f1f86bc0df56e56699bbb7c4449c8cd4514ec5
Tomasz Sztokinier <tomasz@todoit.pl>
2025-09-17 11:54:33 +0200

    finalizer exception PoC

---
+++ FinalizerTest/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+for (int i = 0; i < 3; i++) {
+    new Foobar().Baz();
+}
+Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId}");
+Console.WriteLine("type 'exit' to quit");
+string res;
+do {
+    res = Console.ReadLine() ?? "";
+    GC.Collect();
+} while(res != "exit");
+
+public class Foobar {
+    public void Baz() {
+        
+    }
+    
+    ~Foobar() {
+        Console.WriteLine($"finalizer thread running: {Thread.CurrentThread.ManagedThreadId}");
+        throw new Exception("test");
+    }
+}
