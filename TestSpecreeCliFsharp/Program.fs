d8fd9c8bbe98cb127c83ea014f039f95d25a58ce
Tomasz Sztokinier <tomasz@todoit.pl>
2026-02-10 15:40:37 +0100

    F# with specrtre console PoC

---
+++ TestSpecreeCliFsharp/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+open Spectre.Console
+open Spectre.Console.Cli
+
+type HelloSettings() =
+    inherit CommandSettings()
+    
+    [<CommandOption("--text|-t")>]
+    member val Text:string|null = null with get, set
+
+
+type HelloCommand() =
+    inherit Command<HelloSettings>()
+    
+    override _.Execute (context: CommandContext, settings: HelloSettings, cancellationToken: System.Threading.CancellationToken): int =
+        let text =
+            match settings.Text with
+            | Null -> "Hello world"
+            | NonNull x -> x
+        
+        AnsiConsole.MarkupInterpolated $"Saying: [bold]{text}[/]"
+        
+        0
+
+let app = CommandApp()
+app.Configure(fun cfg ->
+    cfg.AddCommand<HelloCommand>("hello")
+    |> ignore)
+
+Environment.ExitCode <- app.Run(Environment.GetCommandLineArgs()[1..])
