960d87c82507dfcba60d81bd52529f291b030543
Tomasz Sztokinier <tomasz.sztokinier@greenyard.group>
2026-08-04 12:18:47 +0200

    MS Logging PoC/research app

---
+++ MsLoggingPoc/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
+
+Console.WriteLine("Hello, World!");
+
+
+var lf = LoggerFactory.Create(builder => {
+    builder.AddConsole().SetMinimumLevel(LogLevel.Trace);
+});
+var logger = lf.CreateLogger<Program>();
+
+var sb = new StringBuilder();
+sb.Append("the").Append(" ").Append("log");
+logger.LogInformation("{contents}", sb);
