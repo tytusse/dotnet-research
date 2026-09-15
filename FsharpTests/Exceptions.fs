1d4026092b08e24ce2bcf1e8ad899ee0989b0f49
Tomasz Sztokinier <tomasz.sztokinier@greenyard.group>
2026-09-15 17:04:39 +0200

    showcase F# exception stack-trace ignoring StackTraceHidden atteibute in some scenarios
    scenario: exception thrown inside "hidden" method; 
    C# is fine (PoC not included)

--- FsharpTests/Exceptions.fs
+++ FsharpTests/Exceptions.fs
@@ -1,14 +1,36 @@
 ﻿module FsharpTests.Exceptions
 
+open System.Diagnostics
+open System.Runtime.InteropServices
 open Xunit
 open Xunit.Abstractions
 
-type TestException(msg:string) =
-    inherit exn(msg)
+type TestException(msg:string, [<DefaultParameterValue(null); OptionalArgument>]inner:exn|null) =
+    inherit exn(msg, inner)
     new() = TestException("test error")
     
 type FailInProperty() =
     member val Foobar : string = raise(TestException())
+
+let inline someInline f = f()
+
+[<StackTraceHidden>]
+let someExcluded f = f()
+
+[<StackTraceHidden>]
+let someExcludedCurried f x = f x
+
+[<StackTraceHidden>]
+let someExcludedThrowing f =
+    try f()
+    with x -> raise(TestException("wrapped inner", x))
+
+[<StackTraceHidden>]
+type ExcludeFromTrace() =
+    member _.Run(f) = f()
+    member _.RunThrowing(f) =
+        try f()
+        with x -> raise(TestException("rethrow", x))
 
 type Fixture(output:ITestOutputHelper) =
     [<Fact>]
@@ -26,3 +48,26 @@ type Fixture(output:ITestOutputHelper) =
     member _.``val property exception will be thrown from constructor``() : unit =
         let exc = Assert.Throws<TestException>(fun () -> FailInProperty() |> ignore)
         output.WriteLine $"Exception: {exc}"
+        
+    [<Fact>]
+    member _.``stack trace behavior``() : unit =
+        let excludeMeths = ExcludeFromTrace()
+        let fail() : unit =
+            excludeMeths.Run(fun() ->
+                someInline(fun() ->
+                    someExcluded(fun() ->
+                        "A" |> someExcludedCurried(fun x ->
+                            someExcludedThrowing(fun ()->
+                                raise(TestException($"foo: {x}")))))))
+            
+        let fail2() : unit =
+            excludeMeths.Run(fun() ->
+                excludeMeths.Run(fun() ->
+                    excludeMeths.RunThrowing(fun ()->
+                        raise(TestException("foo")))))
+ 
+        let exc = Assert.Throws<TestException>(fail)
+        output.WriteLine $"Exception: {exc}"
+        let exc2 = Assert.Throws<TestException>(fail2)
+        output.WriteLine $"Exception: {exc2}"
+        
